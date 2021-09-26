f1c181a945bcf904768a8b572fa59bacdc40dfe6
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-09-26 14:22:25 +0530

    create IBOutlet and changing image properties dynamically

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
+    
+    // To create the IBOutlet select the UI element press control and drag the mouse to view controller
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    // Calls when view loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Change the image inside the image view
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix");
+        diceImageView1.alpha = 0.5;
+        
+        // Change the opacity of the image view
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo");
     }
 
 
